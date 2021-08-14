e1b2cdcc3c418ef0e7c50ea2059f183be3ef1776
MustafaAbuGhannam <mustafa99au@gmail.com>
2021-08-14 18:20:29 +0300

    started working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ProjectIdea from "./componentes/ProjectIdea";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <div className='container'>
+            <h1 className='projectTitle'>العنوان</h1>
+            <div className='selectSection'>
+              <Link to='/projectIdea'> فكرة المشروع</Link>
+              <Link to='/Book'>الرسائل </Link>
+            </div>
+          </div>
+        </Route>
+        <Route exact path='/projectIdea'>
+          <ProjectIdea />
+        </Route>
+        <Route exact path='/Book'></Route>
+      </Switch>
+    </Router>
   );
 }
 
