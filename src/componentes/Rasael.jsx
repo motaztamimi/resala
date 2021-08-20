025aef09214105c155655e4c746146995e9f353e
MustafaAbuGhannam <mustafa99au@gmail.com>
2021-08-20 19:14:22 +0300

    added search functionality

--- src/componentes/Rasael.jsx
+++ src/componentes/Rasael.jsx
@@ -4,7 +4,23 @@ import BookElement from "./BookElement";
 import "../styles/bookelemnt.css";
 const Rasael = () => {
   const [files, setFiles] = useState(null);
-
+  const [searchResult, setSearchResult] = useState(null);
+  const onSearchInputChange = (serachStr) => {
+    const temp = [];
+    if (serachStr === "") {
+      setSearchResult(files);
+      return;
+    }
+    files.forEach((file) => {
+      if (
+        file.fileName.includes(serachStr) ||
+        file.fileAuther.includes(serachStr)
+      ) {
+        temp.push(file);
+      }
+    });
+    setSearchResult(temp);
+  };
   useEffect(() => {
     dataBase
       .collection("pdfs")
@@ -16,15 +32,25 @@ const Rasael = () => {
           temp.push(item.data());
         });
         setFiles(temp);
+        setSearchResult(temp);
       });
   }, []);
-
   return (
-    <div className='pdffiles'>
-      {files &&
-        files.map((item, index) => {
-          return <BookElement doc={item} key={index}></BookElement>;
-        })}
+    <div className='rsaelPage'>
+      <input
+        type='search'
+        className='searchInput'
+        placeholder='بحث'
+        onChange={(e) => {
+          onSearchInputChange(e.target.value);
+        }}
+      />
+      <div className='pdffiles'>
+        {searchResult &&
+          searchResult.map((item, index) => {
+            return <BookElement doc={item} key={index}></BookElement>;
+          })}
+      </div>
     </div>
   );
 };
