62982c800a17df72b8fc57cc48a5b5e8e5e7d091
motaztamimi <74739600+motaztamimi@users.noreply.github.com>
2021-09-11 20:09:00 +0300

    update pdf file

--- src/componentes/BookElement.jsx
+++ src/componentes/BookElement.jsx
@@ -1,14 +1,12 @@
 import "../styles/bookelemnt.css";
-import imge from "../componentes/book.png";
-const BookElement = ({ doc }) => {
+import imge from "../componentes/JvzCve.png";
+const BookElement = ({ doc ,ind}) => {
   return (
-    <div className='eachpdf'>
+    <div className={`eachpdf ${ind%2==0 ? "shadow": ""}`} >
       <img src={imge} alt='' />
-      <div className='containerr'>
-        <h5>{doc.fileName}</h5>
-        <h5>{doc.fileAuther}</h5>
-      </div>
-      <div></div>
+        <h5 className='containerr'> {doc.fileName}</h5>
+        <h5 className='containerr'>{doc.fileAuther}</h5>
+     
       <button>
         <a href={doc.fileLink}>للتصفح والتنزيل</a>
       </button>
