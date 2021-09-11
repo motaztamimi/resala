de8402ee548f30e858929fce602b1e9add230062
motaztamimi <74739600+motaztamimi@users.noreply.github.com>
2021-09-11 17:50:02 +0300

    update the home page from  zero motaz work

--- src/App.js
+++ src/App.js
@@ -2,24 +2,13 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import ProjectIdea from "./componentes/ProjectIdea";
 import Rasael from "./componentes/Rasael";
 import SearchRsael from './componentes/SearchRsael';
+import MainPagee from "./componentes/MainPagee";
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path='/'>
-          <div className='container'>
-            <h1 className='projectTitle'>
-              تحقيق كتاب فتح القدير للكمال بن الهمام
-            </h1>
-            <h1 className='projectTitle'>
-              على الهداية شرح بداية المبتدي لبرهان الدين المرغيناني
-            </h1>
-            <div className='selectSection'>
-              <Link to='/projectIdea'> فكرة المشروع</Link>
-              <Link to='/Rasael'>تفصح وتحميل الرسائل </Link>
-              <Link to='/table'>كشف بأسماء الرسايل</Link>
-            </div>
-          </div>
+        <MainPagee/>
         </Route>
         <Route exact path='/projectIdea'>
           <ProjectIdea />
