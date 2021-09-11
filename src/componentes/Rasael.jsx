4fc86a2e4a5f652885c3c423734bf0c91febe36e
MustafaAbuGhannam <mustafa99au@gmail.com>
2021-09-11 20:17:33 +0300

    fixed another stupied bugs made by motaz

--- src/componentes/Rasael.jsx
+++ src/componentes/Rasael.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { dataBase } from "../config/firebase";
 import BookElement from "./BookElement";
-import "../index.css"
+import "../index.css";
 import "../styles/bookelemnt.css";
 const Rasael = () => {
   const [files, setFiles] = useState(null);
@@ -14,7 +15,6 @@ const Rasael = () => {
     }
     files.forEach((file) => {
       if (
-        
         file.fileName.includes(serachStr) ||
         file.fileAuther.includes(serachStr)
       ) {
@@ -39,7 +39,7 @@ const Rasael = () => {
   }, []);
   return (
     <div className='rsaelPage'>
-       <div className='toptext'>
+      <div className='toptext'>
         <h4 className='projectTitle'>تحقيق كتاب</h4>
         <h3 className='projectTitle'>
           {" "}
@@ -48,24 +48,26 @@ const Rasael = () => {
 
         <h4 className='projectTitle'>لبرهان الدين المرغيناني</h4>
       </div>
- 
-      <div className ="topic">
-      <input
-        type='search'
-        className='searchInput'
-        placeholder='بحث...'
-        onChange={(e) => {
-          onSearchInputChange(e.target.value);
-        }}/>
-      <h1>الرسائل</h1>
-         <button>العودة الى الصفحة الرئيسية</button>
+
+      <div className='topic'>
+        <input
+          type='search'
+          className='searchInput'
+          placeholder='بحث...'
+          onChange={(e) => {
+            onSearchInputChange(e.target.value);
+          }}
+        />
+        <h1>الرسائل</h1>
+        <Link to='/'>العودة الى الصفحة الرئيسية</Link>
       </div>
-     
+
       <div className='pdffiles'>
-      
         {searchResult &&
           searchResult.map((item, index) => {
-            return <BookElement doc={item} key={index} ind={index}></BookElement>;
+            return (
+              <BookElement doc={item} key={index} ind={index}></BookElement>
+            );
           })}
       </div>
     </div>
