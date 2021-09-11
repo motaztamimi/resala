ffbfc8ff9250ac078e1046100b4189b330351668
MustafaAbuGhannam <mustafa99au@gmail.com>
2021-09-11 20:13:56 +0300

    fix motaz stupied bugs

--- src/componentes/BookElement.jsx
+++ src/componentes/BookElement.jsx
@@ -1,12 +1,12 @@
 import "../styles/bookelemnt.css";
 import imge from "../componentes/JvzCve.png";
-const BookElement = ({ doc ,ind}) => {
+const BookElement = ({ doc, ind }) => {
   return (
-    <div className={`eachpdf ${ind%2==0 ? "shadow": ""}`} >
+    <div className={`eachpdf ${ind % 2 === 0 ? "shadow" : ""}`}>
       <img src={imge} alt='' />
-        <h5 className='containerr'> {doc.fileName}</h5>
-        <h5 className='containerr'>{doc.fileAuther}</h5>
-     
+      <h5 className='containerr'> {doc.fileName}</h5>
+      <h5 className='containerr'>{doc.fileAuther}</h5>
+
       <button>
         <a href={doc.fileLink}>للتصفح والتنزيل</a>
       </button>
