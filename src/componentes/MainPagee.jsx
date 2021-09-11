0f718ceb1b4465bdab6c0f9813d9a2f13b553c10
MustafaAbuGhannam <mustafa99au@gmail.com>
2021-09-11 18:03:06 +0300

    fixed some issue

--- src/componentes/MainPagee.jsx
+++ src/componentes/MainPagee.jsx
@@ -1,38 +1,28 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import "../index.css"
-import img1 from "./Asset 8.png"
+import "../index.css";
+import img1 from "./Asset 8.png";
 
 const MainPagee = () => {
-    return (
-        <div className='container'>
-            <div className="toptext">
-                
-       
-            <h4 className='projectTitle'>
-            تحقيق كتاب
-            </h4>
-        <h3 className='projectTitle'>
-          فتح القدير للكمال بن الهمام
-        </h3>
-      
-        <h3 className='projectTitle'>
-          على الهداية شرح بداية المبتدي
-        </h3>
-        <h4 className='projectTitle'>
-        لبرهان الدين المرغيناني
-            </h4>
-            </div>
-        <div className='selectSection'>
-          <Link to='/projectIdea'> فكرة المشروع</Link>
-          <Link to='/Rasael'>التعريف بالمشرف على المشروع</Link>
-          <Link to='/Rasael'>تصفح وتحميل الرسائل </Link>
-          <Link to='/table'>كشف بأسماء الرسايل</Link>
-        </div>
-       
-        <img  id ="imageHomepage"src={img1} alt="" />
-      </div>
-    )
-}
+  return (
+    <div className='container'>
+      <div className='toptext'>
+        <h4 className='projectTitle'>تحقيق كتاب</h4>
+        <h3 className='projectTitle'>فتح القدير للكمال بن الهمام</h3>
 
-export default MainPagee
+        <h3 className='projectTitle'>على الهداية شرح بداية المبتدي</h3>
+        <h4 className='projectTitle'>لبرهان الدين المرغيناني</h4>
+      </div>
+      <div className='selectSection'>
+        <Link to='/projectIdea'> فكرة المشروع</Link>
+        <Link to='/Projectadministrator'>التعريف بالمشرف على المشروع</Link>
+        <Link to='/Rasael'>تصفح وتحميل الرسائل </Link>
+        <Link to='/table'>كشف بأسماء الرسايل</Link>
+      </div>
+
+      <img id='imageHomepage' src={img1} alt='' />
+    </div>
+  );
+};
+
+export default MainPagee;
